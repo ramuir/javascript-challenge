39ab071bee4f3cd6bdc156ada655ecf72100e5d7
ramuir <65628879+ramuir@users.noreply.github.com>
2020-11-08 22:18:26 -0600

    finished level 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -2,3 +2,42 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// vairable for tbody
+var tbody = d3.select("tbody");
+
+// get values for each column and row
+tableData.forEach((ufoReport)=>{
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key,value]) =>{
+    console.log(key,value);
+    var cell = row.append("td");
+    cell.text(value);  
+    });
+});
+// Select button
+var button = d3.select("#filter-btn")
+button.on("click", function() {
+    tbody.html("");
+    var inputRequest = d3.select("#input");
+    var inputValue = inputRequest.property("value");
+    var filteredData = tableData.filter(ufoSightings => 
+        ufoSightings.datetime === inputValue ||
+        ufoSightings.city === inputValue ||
+        ufoSightings.state === inputValue ||
+        ufoSightings.country === inputValue ||
+        ufoSightings.shape === inputValue
+        );
+        console.log(filteredData);
+
+        filteredData.forEach(function(inputChoice){
+            console.log(inputChoice);
+    
+            var row = tbody.append("tr");
+            Object.entries(inputChoice).forEach(function([key, value]) {
+                console.log(key, value);
+                var cell = row.append("td");
+                cell.text(value);
+
+});
+});
+});
